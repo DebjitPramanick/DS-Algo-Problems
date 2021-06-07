a00cca36ea6312f77521141b716fb1c690bc8f89
DebjitPramanick <dpramanick9990@gmail.com>
2021-06-07 23:04:40 +0530

    Graph BFS

---
+++ DS Problems/Graph/Traversal.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void BFS(int n, vector<int> adj[])
+{
+    vector<int> bfs;
+    vector<int> vis(n+1,0);
+
+    for(int i=1;i<n;i++){
+
+        if(vis[i] == 0){
+
+            // BFS Code
+
+            queue<int> q;
+            q.push(i);
+            vis[i] = 1;
+
+            while(!q.empty()){
+                int node = q.front();
+                q.pop();
+                bfs.push_back(node);
+
+                for(auto it: adj[node]){
+                    if(!vis[it]){
+                        q.push(it);
+                        vis[it] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    for(auto i: bfs){
+        cout << i << " ";
+    }
+}
+
+void adjList(int n, int m)
+{
+
+    vector<int> adj[n + 1];
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cout << "Enter value of u (1st node): ";
+        cin >> u;
+        cout << "Enter value of v (2nd node): ";
+        cin >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    BFS(n, adj);
+}
+
+int main()
+{
+    cout << "Traversal of graph: "
+         << "\n";
+
+    int n, m;
+    cout << "Enter value of n (no. of nodes): ";
+    cin >> n;
+    cout << "Enter value of m (no. of edges): ";
+    cin >> m;
+
+    adjList(n, m);
+}
