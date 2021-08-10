827b589f5dc49efb4f75710a17f5989413b053aa
DebjitPramanick <dpramanick9990@gmail.com>
2021-08-10 08:50:19 +0530

    Quick Sort

---
+++ Algo Problems/Sorting/quickSort.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int partition(int a[], int l, int r){
+    int pivot = a[r];
+    int i = l-1;
+    for(int j=l;j<r;j++){
+        if(a[j]<pivot){
+            i++;
+            swap(a[i], a[j]);
+        }
+    }
+    swap(a[i+1], a[r]);
+    cout<<i+1<<"\t";
+    return i+1;
+}
+
+void quickSort(int a[], int l, int r){
+    if(l<r){
+        int pi = partition(a, l, r);
+        quickSort(a, l, pi-1);
+        quickSort(a, pi+1, r);
+    }
+}
+
+int main(){
+    int a[] = {4,2,9,6,1,3};
+    quickSort(a, 0, 5);
+    cout<<endl;
+    for(int i=0;i<6;i++){
+        cout<<a[i]<<"  ";
+    }
+}
