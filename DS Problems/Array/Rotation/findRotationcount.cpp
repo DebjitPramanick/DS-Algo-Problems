c3d76151284c510a54f878034ffd345c797fc42a
DebjitPramanick <dpramanick9990@gmail.com>
2021-08-27 16:43:36 +0530

    Find rotation count

---
+++ DS Problems/Array/Rotation/findRotationcount.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int findRotation(int a[], int n){
+    int s = 0, e = n;
+    int c = 0;
+    while (s<=e)
+    {
+        int mid = (s+e)/2;
+        int nxt = (mid+1)%n;
+        int prev = (mid+n-1)%n;
+        if(a[mid]<a[nxt] and a[mid]<a[prev]){
+            c =  mid;
+            break;
+        }
+        else if(a[s]<=a[mid]) s=mid+1;
+        else e=mid-1;
+    }
+    return n-c; // For anti-clock wise rotation
+    // return c; // For clock wise rotation
+}
+
+
+int main(){
+    int a[] = {15, 18, 2, 3, 6, 12};
+    int n =sizeof(a)/sizeof(a[0]);
+    int res = findRotation(a,n);
+    cout<<res;
+}
+
