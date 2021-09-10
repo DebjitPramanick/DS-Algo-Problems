da8b3dbb2dc3efeda0544bacb3b40d15f402cb1f
DebjitPramanick <dpramanick9990@gmail.com>
2021-09-10 07:31:09 +0530

    Print middle node

---
+++ DS Problems/Linked List/Single Linked List/printMiddleNode.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+	public:
+		int val;
+		Node *next;
+		Node(){
+			next = NULL;
+		}
+};
+
+
+class LinkedList {
+    Node *head;
+    public:
+        LinkedList(){
+            head = NULL;
+        }
+        
+        void insertNode(int v){
+            Node *newNode = new Node();
+            newNode->val = v;
+            newNode->next = NULL;
+            if(!head){
+                head = newNode;
+                cout<<"Item inserted."<<endl;
+            }
+            else{
+                Node *temp = head;
+                while(temp->next)
+                    temp = temp->next;
+                temp->next = newNode;
+                cout<<"Item inserted."<<endl;
+            }
+        }
+
+        void deleteNode(int v){
+            if(!head){
+                cout<<"List is empty."<<endl;
+                return;
+            }
+            Node *temp = head;
+            if(temp->val== v){
+                head = temp->next;
+                temp->next = NULL;
+                delete(temp);
+                cout<<"List item deleted."<<endl;
+                return;
+            }
+            Node *prev = temp;
+            temp = temp->next;
+            while(temp){
+                if(temp->val == v){
+                    prev->next = temp->next;
+                    temp->next = NULL;
+                    delete(temp);
+                    cout<<"List item deleted."<<endl;
+                    return;
+                }
+                prev = temp;
+                temp = temp->next;
+            }
+        }
+
+        int searchNode(int v){
+            if(!head) return -1;
+            Node *temp = head;
+            int i = 0;
+            while(temp){
+                i++;
+                if(temp->val== v) break;
+                temp = temp->next;
+            }
+            return i;
+        }
+
+        void display(){
+            if(!head) {
+                cout<<"List is empty."<<endl;
+                return;
+            }
+            Node *temp = head;
+            int i = 0;
+            while(temp){
+                i++;
+                cout<<temp->val<<" ";
+                temp = temp->next;
+            }
+            cout<<endl<<"Node count: "<<i<<endl;
+        }
+
+        void printMiddle(){
+            if(!head){
+                cout<<"List is empty."<<endl;
+                return;
+            }
+            Node *first = head;
+            Node *second = head;
+            while(second and second->next){
+                first = first->next;
+                second = second->next->next;
+            }
+            cout<<"Middle element is: "<<first->val<<endl;
+        }
+
+};
+
+int main(){
+    LinkedList l;
+    for(int i=0;i<5;i++){
+        l.insertNode(5*(i+1));
+    }
+
+    l.display();
+    l.deleteNode(15);
+    l.deleteNode(25);
+    l.display();
+    l.insertNode(34);
+    l.insertNode(56);
+    l.display();
+    cout<<"Item is: "<<l.searchNode(34)<<"th element."<<endl;
+    l.printMiddle();
+}
+
