ef7fe03bbe70237e3be9ace31904c49e9485b61b
DebjitPramanick <dpramanick9990@gmail.com>
2021-11-23 21:41:11 +0530

    Code bugs fixed

---
+++ Algo Problems/Binary Search/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+
+    // int n = sizeof(a)/sizeof(a[0]);
+    // int res, s;
+    // cout<<"Enter element to search: ";
+    // cin>>s;
+    // res = findEl(a,n,s);
+    // if(res != -1) cout<<"Element found at: "<<res;
+    // else cout<<"Element not found.";
