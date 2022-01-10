e01a43d03fd316dc70072a5ff8faf7ef37f4df02
DebjitPramanick <dpramanick9990@gmail.com>
2022-01-10 11:40:16 +0530

    Explanation

--- Algo Problems/Greedy/Buy & Sell Stocks/buyNSell_Transaction_Fees.cpp
+++ Algo Problems/Greedy/Buy & Sell Stocks/buyNSell_Transaction_Fees.cpp
@@ -2,22 +2,31 @@
 using namespace std;
 
 // Buy and sell stocks with transaction fees
+// Explanation: Pepcoding
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int obsp=-prices[0];
-        int ossp=0;
+        int obsp=-prices[0]; // Old buy state
+        int ossp=0; // Old sell state
         for(int i=1;i<prices.size();i++)
         {
-            int nbsp=0;
-            int nssp=0;
+            int nbsp=0; // New buy state
+            int nssp=0; // New sell state
             
             if(ossp-prices[i]>obsp)
+                /* 
+                   If new buy state is better than the old buy state
+                   with the profit of old sell state
+                */
                 nbsp=ossp-prices[i];
             else
-                nbsp=obsp;
-            if(obsp+prices[i]-fee>ossp)
+                nbsp=obsp; // Else make new buy state as old buy state
+            if(obsp+prices[i]-fee>ossp) 
+                /* 
+                   If new sell state (profit for selling stocks - fees) 
+                   is better than the old sell state
+                */
                 nssp=obsp+prices[i]-fee;
             else
                 nssp=ossp;
