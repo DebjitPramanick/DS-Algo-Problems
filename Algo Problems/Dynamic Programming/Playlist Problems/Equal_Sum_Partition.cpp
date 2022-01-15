23bd1b44183afd30d64bab875d448047fb812b87
DebjitPramanick <dpramanick9990@gmail.com>
2022-01-15 12:33:30 +0530

    Equal sum partition

--- Algo Problems/Dynamic Programming/Playlist Problems/Equal_Sum_Partition.cpp
+++ Algo Problems/Dynamic Programming/Playlist Problems/Equal_Sum_Partition.cpp
@@ -2,44 +2,41 @@
 using namespace std;
 
 /*
-Given an array of integers and a target sum. We have to find if
-there exists a subset whose sum is equal to target sum.
+Given an array of integers. We have to find if
+the array can have two parts whose sums are equal.
 */
 
-class ZeroOneKnapsack{
+class EqualSumPartition{
     private:
-        vector<int> nums = {2,3,7,8,10};
-        int target = 8;
+        vector<int> nums = {1,5,11,5};
     public:
 
         // Recursion -------------------------------------------------
 
-        bool recur(vector<int> &v, int t, int n){
+        bool recur(vector<int> &v, int n, int t){
             // Base case
-
             if(t==0) return true;
             else if(n==0) return false;
-
+            
             // Code for different choices
 
             if(v[n-1]<=t){
-                return recur(v, t-v[n-1], n-1) || recur(v, t, n-1);
+                return recur(v, n-1, t-v[n-1]) || recur(v, n-1, t);
             }
-            if(v[n-1]>t){
-                return recur(v, t, n-1);
+            else if(v[n-1]>t) {
+                return recur(v, n-1, t);
             }
 
             return false;
+
         }
 
         // Top-Down -------------------------------------------------
 
-        bool topDown(){
+        bool topDown(int n, int t){
             // Base case
 
-            int n = nums.size(), t = target;
             int dp[n+1][t+1];
-
             for(int i=0;i<n+1;i++){
                 for(int j=0;j<t+1;j++){
                     if(j==0) dp[i][j] = true;
@@ -51,32 +48,34 @@ class ZeroOneKnapsack{
 
             for(int i=1;i<n+1;i++){
                 for(int j=1;j<t+1;j++){
-                    if(nums[i-1]<=j){
-                        dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                    }
-                    if(nums[i-1]>j){
-                        dp[i][j] = dp[i-1][j];
-                    }
+                    if(nums[i-1]<=j) dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                    if(nums[i-1]>j) dp[i][j] = dp[i-1][j];
                 }
             }
 
             return dp[n][t];
+            
         }
 
 
         void solve(){
 
             bool ans;
-            int n = nums.size();
-            ans = recur(nums, target, n);
+            int n = nums.size(), sum=0;
+            for(int i=0;i<n;i++) sum+=nums[i];
+            if(sum%2!=0) {
+                cout<<"No"<<endl;
+                return;
+            }
+            ans = recur(nums, n, sum/2);
             ans? cout<<"Yes"<<endl : cout<<"No"<<endl;
-            ans = topDown();
+            ans = topDown(n, sum/2);
             ans? cout<<"Yes"<<endl : cout<<"No"<<endl;
         }
 
 };
 
 int main(){
-    ZeroOneKnapsack s;
+    EqualSumPartition s;
     s.solve();
 }
