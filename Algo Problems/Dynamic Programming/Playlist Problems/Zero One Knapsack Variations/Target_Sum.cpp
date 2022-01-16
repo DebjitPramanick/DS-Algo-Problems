0155d9fdf7d512013e9e641a45c0eb537671534b
DebjitPramanick <dpramanick9990@gmail.com>
2022-01-16 21:11:11 +0530

    Modified target sum problem for certain base cases

--- Algo Problems/Dynamic Programming/Playlist Problems/Zero One Knapsack Variations/Target_Sum.cpp
+++ Algo Problems/Dynamic Programming/Playlist Problems/Zero One Knapsack Variations/Target_Sum.cpp
@@ -24,17 +24,14 @@ class TargetSum{
             int n = nums.size();
             int dp[n+1][t+1];
 
-            for(int i=0;i<n+1;i++){
-                for(int j=0;j<t+1;j++){
-                    if(j==0) dp[i][j] = 1;
-                    else if(i==0) dp[i][j] = 0;
-                }
-            }
+            dp[0][0] = 1;
+            for(int i=1;i<t+1;i++)
+                dp[0][i] = 0;
 
             // Code for different choices
 
             for(int i=1;i<n+1;i++){
-                for(int j=1;j<t+1;j++){
+                for(int j=0;j<t+1;j++){
                     if(nums[i-1]<=j){
                         dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
                     }
@@ -54,6 +51,10 @@ class TargetSum{
             int n = nums.size();
             int sum = 0;
             for(int i=0;i<n;i++) sum+=nums[i];
+            if (abs(target) > sum || (sum + target) % 2 != 0){ 
+                // For handling certain testcases
+                return;
+            }
             int t = (target+sum)/2;
             ans = getSubsetSumCount(t);
             cout<<"No. of combinations is: "<<ans<<endl;
