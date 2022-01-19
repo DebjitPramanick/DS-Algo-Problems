c47680ca93b03714d4725483d3f23d13382e17e0
DebjitPramanick <dpramanick9990@gmail.com>
2022-01-19 20:52:47 +0530

    LCS Topdown

--- Algo Problems/Dynamic Programming/Playlist Problems/Longest Common Subsequence Pattern/LCS.cpp
+++ Algo Problems/Dynamic Programming/Playlist Problems/Longest Common Subsequence Pattern/LCS.cpp
@@ -43,11 +43,36 @@ class LCS{
             return dp[m][n] = 1+rec(m-1, n-1);
         }
 
+        // Top-Sown ---------------------------------------
+        int topdown(int m, int n){
+            // Base case
+            int t[m+1][n+1];
+            for(int i=0;i<m+1;i++){
+                for(int j=0;j<n+1;j++){
+                    if(i==0 || j==0) t[i][j] = 0;
+                }
+            }
+
+            // Code for different choices
+            for(int i=1;i<m+1;i++){
+                for(int j=1;j<n+1;j++){
+                    if(x[i-1]!=y[j-1]) 
+                        t[i][j] = max(t[i-1][j], t[i][j-1]);
+            
+                    else t[i][j] = 1+t[i-1][j-1];
+                }
+            }
+
+            return t[m][n];
+        }
+
         void solve(){
             int ans;
             ans = rec(x.size(), y.size());
             cout<<"LCS length is: "<<ans<<endl;
             ans = memoization(x.size(), y.size());
+            cout<<"LCS length is: "<<ans<<endl;
+            ans = topdown(x.size(), y.size());
             cout<<"LCS length is: "<<ans<<endl;
         }
 
