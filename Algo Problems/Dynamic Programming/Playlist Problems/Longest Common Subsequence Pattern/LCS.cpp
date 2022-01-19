a5b4410571597e0224c5c1dbd622b4a4468c77ac
DebjitPramanick <dpramanick9990@gmail.com>
2022-01-19 20:44:09 +0530

    LCS Memoization

--- Algo Problems/Dynamic Programming/Playlist Problems/Longest Common Subsequence Pattern/LCS.cpp
+++ Algo Problems/Dynamic Programming/Playlist Problems/Longest Common Subsequence Pattern/LCS.cpp
@@ -11,6 +11,7 @@ s2: abedfhr
 LCS: abdh
 */
 
+int dp[1002][1002];
 
 class LCS{
     private:
@@ -29,10 +30,24 @@ class LCS{
             return 1+rec(m-1, n-1);
         }
 
+        // Memoization ---------------------------------------
+        int memoization(int m, int n){
+            // Base case
+            if(m==0 || n==0) return 0;
+
+            // Code for different choices
+            if(dp[m][n]!=-1) return dp[m][n];
+            if(x[m-1]!=y[n-1]) 
+                return dp[m][n] = max(rec(m-1, n), rec(m, n-1));
+            
+            return dp[m][n] = 1+rec(m-1, n-1);
+        }
+
         void solve(){
             int ans;
             ans = rec(x.size(), y.size());
-            
+            cout<<"LCS length is: "<<ans<<endl;
+            ans = memoization(x.size(), y.size());
             cout<<"LCS length is: "<<ans<<endl;
         }
 
@@ -40,5 +55,6 @@ class LCS{
 
 int main(){
     LCS s;
+    memset(dp, -1, sizeof(dp));
     s.solve();
 }
