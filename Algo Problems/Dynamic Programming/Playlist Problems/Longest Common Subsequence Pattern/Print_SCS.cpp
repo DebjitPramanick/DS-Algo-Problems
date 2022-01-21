7ee74b60af48d3abd3fa1941ac1195e8e4f5cce6
DebjitPramanick <dpramanick9990@gmail.com>
2022-01-21 11:37:37 +0530

    Print SCS

--- Algo Problems/Dynamic Programming/Playlist Problems/Longest Common Subsequence Pattern/Print_SCS.cpp
+++ Algo Problems/Dynamic Programming/Playlist Problems/Longest Common Subsequence Pattern/Print_SCS.cpp
@@ -11,13 +11,13 @@ s2: eke
 SCS: geeke <- Supersequence (Both s1 and s2 are present)
 */
 
-class SCS{
+class PrintSCS{
     private:
         string x = "geek", y = "eke";
     public:
 
         // Top-Down ---------------------------------------
-        int topdown(int m, int n){
+        int topdown(int m, int n, string &scs){
 
             // Base case
             int t[m+1][n+1];
@@ -37,23 +37,57 @@ class SCS{
                 }
             }
 
+            cout<<scs<<endl;
+
+            int i = m, j = n;
+            while(i>0 and j>0){
+                if(x[i-1]==y[j-1]){
+                    scs.push_back(x[i-1]);
+                    i--;
+                    j--;
+                }else {
+                    if(t[i][j-1]>t[i-1][j]){
+                        scs.push_back(y[j-1]);
+                        j--;
+                    }
+                    if(t[i][j-1]<t[i-1][j]){
+                        scs.push_back(x[i-1]);
+                        i--;
+                    }
+                }
+                cout<<scs<<endl;
+            }
+
+            cout<<scs<<endl;
+
+            while(i>0){
+                scs.push_back(x[i-1]);
+                i--;
+            }
+            while(j>0){
+                scs.push_back(y[j-1]);
+                j--;
+            }
+
             return t[m][n];
         }
 
         void solve(){
             int ans;
-            ans = topdown(x.size(), y.size()); // Length of LCS
+            string scs = "";
+            ans = topdown(x.size(), y.size(), scs); // Length of LCS
             // We will get the max length of scs by simply
             // merging both the strings
             // Then we will substract the length of lcs 
             // to get min length of scs
             ans = (x.size()+y.size()) - ans;
             cout<<"SCS length is: "<<ans<<endl;
+            cout<<"SCS is: "<<scs<<endl;
         }
 
 };
 
 int main(){
-    SCS s;
+    PrintSCS s;
     s.solve();
 }
