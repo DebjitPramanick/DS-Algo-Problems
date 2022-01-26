6fc6e6e7c910966a26dac6a9a623a6d0859dba15
DebjitPramanick <dpramanick9990@gmail.com>
2022-01-26 12:05:42 +0530

    MCM recursion

--- Algo Problems/Dynamic Programming/Playlist Problems/Matrix Chain Multiplication/MCM.cpp
+++ Algo Problems/Dynamic Programming/Playlist Problems/Matrix Chain Multiplication/MCM.cpp
@@ -2,50 +2,41 @@
 using namespace std;
 
 /*
-Given a string. Find min number of insertions to
-make a palindrome.
+Given an array p[] which represents the chain of matrices 
+such that the ith matrix Ai is of dimension p[i-1] x p[i]. 
+We need to write a function MatrixChainOrder() that should 
+return the minimum number of multiplications needed to 
+multiply the chain. 
 
 Ex:
-a: acbcbda
-Output: True
+a: {40, 20, 30, 10, 30}
+Output: 26000
 */
 
 class MCM{
     private:
-        string x = "acbcbda";
+        vector<int> a = {40, 20, 30, 10, 30};
     public:
 
-        // Top-Down ---------------------------------------
-        int topdown(int m, int n, string y){
+        // Recursion ---------------------------------------
+        int rec(vector<int> &ar, int i, int j){
 
             // Base case
-            int t[m+1][n+1];
-            for(int i=0;i<m+1;i++){
-                for(int j=0;j<n+1;j++){
-                    if(i==0 || j==0) t[i][j] = 0;
-                }
-            }
+            if(i>=j) return 0;
 
             // Code for different choices
-            for(int i=1;i<m+1;i++){
-                for(int j=1;j<n+1;j++){
-                    if(x[i-1]!=y[j-1]) 
-                        t[i][j] = max(t[i-1][j], t[i][j-1]);
-            
-                    else t[i][j] = 1+t[i-1][j-1];
-                }
+            int mn = INT_MAX;
+            for(int k=i;k<=j-1;k++){
+                int temp = rec(ar, i, k) + rec(ar, k+1, j) + ar[i-1]*ar[k]*ar[j];
+                mn = min(mn, temp);
             }
-
-            return t[m][n];
+            return mn;
         }
 
         void solve(){
             int ans;
-            string rev = x;
-            reverse(rev.begin(), rev.end()); // Get second string by reversing the given string
-            ans = topdown(x.size(), rev.size(), rev);
-            int minD = x.size()-ans;
-            cout<<"Min num of insertions is: "<<minD<<endl;
+            ans = rec(a, 1, a.size()-1);
+            cout<<"Min cost is: "<<ans<<endl;
         }
 
 };
