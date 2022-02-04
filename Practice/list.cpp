a764b86d8aee1b8762915685016226dca9303597
DebjitPramanick <dpramanick9990@gmail.com>
2022-02-04 13:37:11 +0530

    List practice

--- Practice/list.cpp
+++ Practice/list.cpp
@@ -25,6 +25,17 @@ class List{
         }
     }
 
+    int count(){
+        Node* temp = head;
+        int c = 0;
+        while (temp) {
+            temp = temp->next;
+            c++;
+        }
+
+        return c;
+    }
+
     void display(){
         Node* temp = head;
         cout<<endl;
@@ -34,14 +45,37 @@ class List{
         }
         cout<<endl;
     }
+
+    Node* search(int k){
+        Node* temp = head;
+        while(temp){
+            if(temp->val == k) return temp;
+            temp = temp->next;
+        }
+        return NULL;
+    }
+
+    void reverse(){
+        Node *temp = head, *p, *q = NULL;
+        while(temp){
+            p = temp->next;
+            temp->next = q;
+            q = temp;
+            temp = p;
+        }
+        head = q;
+    }
+
 };
 
 int main(){
     List l;
     srand(time(NULL));
-    for(int i=0;i<7;i++){
+    for(int i=1;i<=7;i++){
         int b = rand()%10;
-        l.insert(b);
+        l.insert(i*6);
     }
+    l.display();
+    l.reverse();
     l.display();
 }
